c135a655f568cd7312a687ae4e52b900c7e852b4
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-12 22:15:59 -0300

    Removed useless old commented code

--- P_project/core/views.py
+++ P_project/core/views.py
@@ -6,7 +6,3 @@ from pokemons import *
 def index(request):
     data_pokemon = pokemons()
     return render(request, 'index.html', { 'data_pokemon' : data_pokemon})
-
-# def index(request):
-#     dados_pokemons = pokemons()
-#     return render(request, 'index.html', dados_pokemons)
