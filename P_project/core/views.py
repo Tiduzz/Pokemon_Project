0848a8c314ec902949f233ad385b11eaea81dcbb
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-16 20:05:02 -0300

    Added pokemons_weakness def, fixed bug on pokemons_types, added pandas library to manipulate strings.

--- P_project/core/views.py
+++ P_project/core/views.py
@@ -15,9 +15,11 @@ def pokedex(request):
         poke_stats = {'pokemon_base_status' : pokemons_stats(request.POST.get('poke'))}
         poke_types = {'pokemon_types' : pokemons_types(request.POST.get('poke'))}
         poke_png = {'pokemon_png' : pokemons_png(request.POST.get('poke'))}
+        poke_weak = {'pokemon_weakness' : pokemons_weakness(request.POST.get('poke'))}
     data_pokemon.update(pokemon)
     data_pokemon.update(poke_desc)
     data_pokemon.update(poke_stats)
     data_pokemon.update(poke_types)
     data_pokemon.update(poke_png)
+    data_pokemon.update(poke_weak)
     return render(request, 'pokedex.html', { 'data_pokemon' : data_pokemon })
--- P_project/pokemons.py
+++ P_project/pokemons.py
@@ -1,10 +1,11 @@
 import requests
 import pokepy
+import pandas as pd
 
 
 #returns all 151 pokemons as a dict {'pokemon_name':'Pokemon_name'}
 def pokemons_names():
-    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=3')
+    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=151')
     response = response.json()
 
     results = response['results']
@@ -51,12 +52,53 @@ def pokemons_types(pokemon):
     result = pokepy.V2Client().get_pokemon(pokemon)
     pokemon_types = {}
     for i in range(2):
-        x = {}
-        x = {'type{}'.format(i) : result.types[i].type.name.capitalize()}
-        pokemon_types.update(x)
+        try:
+            x = {}
+            x = {'type{}'.format(i) : result.types[i].type.name.capitalize()}
+            pokemon_types.update(x)
+        except IndexError:
+            pass
     return pokemon_types
 
 def pokemons_png(pokemon):
     result = requests.get('https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon))
     result = result.json()
     return result['sprites']['front_default']
+
+def pokemons_weakness(pokemon):
+    type_weaknesses = {'Fairy' : {'Poison', 'Steel'} }, {'Steel' : {'Fire', 'Fight', 'Ground'} }, {'Dark' : {'Fire', 'Bug', 'Fairy'} }, {'Dragon' : {'Ice', 'Dragon', 'Fairy' }}, {'Ghost' : {'Ghost', 'Dark'} }, {'Rock' : {'Water', 'Grass', 'Fight', 'Ground', 'Steel'}}, {'Bug' : {'Fire', 'Flying', 'Rock'} }, {'Psychic' : {'Bug', 'Ghost', 'Dark'} }, {'Flying' : {'Electric', 'Ice', 'Rock'} }, {'Ground' : {'Water', 'Grass', 'Ice'} }, {'Poison' : {'Ground', 'Psychic'} }, {'Fight' : {'Flying', 'Psychic', 'Fairy'} }, {'Ice' : {'Fire', 'Fight', 'Rock', 'Steel'} }, {'Grass' : {'Fire', 'Ice', 'Poison', 'Flying', 'Bug'} }, {'Electric' : {'Ground'} }, {'Water' : {'Electric', 'Grass'} }, {'Fire' : {'Water', 'Ground', 'Rock'} }, {'Normal' : {'Fight'} }
+    poke_type = pokemons_types(pokemon)
+    type0 = poke_type['type0']
+    try:
+        type1 = poke_type['type1']
+        type0_weak = ''
+        type1_weak = ''
+        double_weak = []
+        count = 0
+        for i in type_weaknesses:
+            for key,value in i.items():
+                if type0 == key:
+                    type0_weak = value
+                if type1 == key:
+                    type1_weak = value
+        for i in type0_weak:
+            for y in type1_weak:
+                if i == y:
+                    double_weak.append(i)
+        pokemon_weak = list(type1_weak)
+        for i in list(type0_weak):
+            pokemon_weak.append(i)
+        pokemon_weak = pd.unique(pokemon_weak).tolist()
+        for i in double_weak:
+            pokemon_weak.remove(i)
+            pokemon_weak.append(i+" x4 DMG!")
+        return pokemon_weak
+
+    except KeyError:
+        type0_weak = ''
+        count = 0
+        for i in type_weaknesses:
+            for key,value in i.items():
+                if type0 == key:
+                    type0_weak = value
+        return list(type0_weak)
